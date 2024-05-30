549834a9ce27698dcb7d4cfae0e31784f974fc9d
Klayni <klayni.milandou@epitech.eu>
2024-05-30 19:03:55 +0200

    :hammer: Implement action methods and actions enum

---
+++ gui/src/trantorians/Actions.cpp
@@ -0,0 +1,66 @@
+/*
+** EPITECH PROJECT, 2024
+** Zappy
+** File description:
+** Actions
+*/
+
+#include "Actions.hpp"
+
+GUI::Actions::Actions()
+{
+}
+
+GUI::Actions::~Actions()
+{
+}
+
+bool GUI::Actions::moveForward()
+{
+}
+
+bool GUI::Actions::turnRight()
+{
+}
+
+bool GUI::Actions::turnLeft()
+{
+}
+
+std::vector<std::vector<int>> GUI::Actions::lookAround()
+{
+}
+
+std::map<std::string, int> GUI::Actions::openInventory()
+{
+}
+
+bool GUI::Actions::broadcastText(std::string text)
+{
+}
+
+int GUI::Actions::connectNbr()
+{
+}
+
+bool GUI::Actions::forkPlayer()
+{
+}
+
+void GUI::Actions::playerDead()
+{
+}
+
+
+bool GUI::Actions::takeObj()
+{
+}
+
+bool GUI::Actions::setObj()
+{
+}
+
+bool GUI::Actions::startIncantation()
+{
+}
+
