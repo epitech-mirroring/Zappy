2a2ec56aac62da4eb69e94dd2458535a300552ea
Baptiste MOREAU <baptiste.moreau@epitech.eu>
2024-05-31 00:44:42 +0200

    :sparkles: Added the GUI world class adding tests soon

---
+++ gui/src/world/World.cpp
@@ -0,0 +1,41 @@
+/*
+** EPITECH PROJECT, 2024
+** Zappy [WSL: kali-linux]
+** File description:
+** World
+*/
+
+#include "World.hpp"
+
+using namespace GUI;
+
+World::World(unsigned int width, unsigned int height)
+    :   _width(width)
+    ,   _height(height)
+{
+}
+
+World::~World()
+{
+}
+
+void World::setWidth(unsigned int width)
+{
+    _width = width;
+}
+
+void World::setHeight(unsigned int height)
+{
+    _height = height;
+}
+
+unsigned int World::getWidth() const
+{
+    return _width;
+}
+
+unsigned int World::getHeight() const
+{
+    return _height;
+}
+
---
+++ gui/src/world/World.hpp
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2024
+** Zappy [WSL: kali-linux]
+** File description:
+** World
+*/
+
+#ifndef WORLD_HPP_
+#define WORLD_HPP_
+
+namespace GUI {
+class World {
+    public:
+        World(unsigned int width, unsigned int height);
+        ~World();
+
+        void setWidth(unsigned int width);
+        void setHeight(unsigned int height);
+
+        unsigned int getWidth() const;
+        unsigned int getHeight() const;
+
+    protected:
+        unsigned int _width;
+        unsigned int _height;
+};
+} // namespace GUI //
+#endif /* !WORLD_HPP_ */
