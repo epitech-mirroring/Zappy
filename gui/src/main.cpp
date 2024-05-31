2f4b90580de305af43c14bcabbef7a3bbc2c91de
Klayni <klayni.milandou@epitech.eu>
2024-05-31 16:08:32 +0200

    :fire: Remove helloworld from main

--- gui/src/main.cpp
+++ gui/src/main.cpp
@@ -1,6 +1,4 @@
 /*
-** EPITECH PROJECT, 2024
-** Zappy
 ** File description:
 ** No file there , just an epitech header example .
 ** You can even have multiple lines if you want !
@@ -12,6 +10,5 @@ int main(int ac, char **av)
 {
     (void)ac;
     (void)av;
-    hello_world();
     return 0;
 }
