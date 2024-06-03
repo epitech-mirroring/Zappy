cf5d6ee27efb1bbe74c78f2237893c03f37983b3
Klayni <klayni.milandou@epitech.eu>
2024-06-03 15:28:54 +0200

    :safety_vest: Add Tribe class and implement methods

---
+++ gui/src/trantorians/Tribes.cpp
@@ -0,0 +1,34 @@
+/*
+** File description:
+** Tribes
+*/
+
+#include "Tribes.hpp"
+
+GUI::Tribes::Tribes()
+{
+}
+
+GUI::Tribes::~Tribes()
+{
+}
+
+void GUI::Tribes::setName(std::string name)
+{
+    _name = name;
+}
+
+std::string GUI::Tribes::getName()
+{
+    return _name;
+}
+
+void GUI::Tribes::addTrantorian(GUI::Trantorian trantorian)
+{
+    _trantorians.emplace_back(trantorian);
+}
+
+std::list<GUI::Trantorian> GUI::Tribes::getTrantorianList()
+{
+    return _trantorians;
+}
---
+++ gui/src/trantorians/Tribes.hpp
@@ -0,0 +1,55 @@
+/*
+** File description:
+** Tribes
+*/
+
+#ifndef TRIBES_
+    #define TRIBES_
+
+    #include <iostream>
+    #include <list>
+    #include "Trantorian.hpp"
+
+namespace GUI
+{
+    class Tribes
+    {
+        public:
+            /**
+             * @brief Create Tribe object
+             */
+            Tribes();
+            /**
+             * @brief Create Tribe object
+             */
+            ~Tribes();
+
+            /**
+             * @brief Set name of the tribe
+             * @param name std::string of tribe's name
+             */
+            void setName(std::string name);
+            /**
+             * @brief Get tribe's name
+             * @return std::string of tribe's name
+             */
+            std::string getName();
+
+            /**
+             * @brief Add new trantorian to tribe
+             * @param Trantorian object
+             */
+            void addTrantorian(GUI::Trantorian trantorian);
+            /**
+             * @brief Get list of trantorian in the tribe
+             * @return list of trantorian objects
+             */
+            std::list<GUI::Trantorian> getTrantorianList();
+
+        private:
+            std::string _name;
+            std::list<GUI::Trantorian> _trantorians;
+    };
+} // namespace GUI
+
+#endif // TRIBES_
