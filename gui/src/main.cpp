62adbd485f4bd6ab3e815a9bbecc5ceda0ea1fad
Klayni <klayni.milandou@epitech.eu>
2024-06-11 15:25:35 +0200

    :package: Link Raylib with zappy executable

--- gui/src/main.cpp
+++ gui/src/main.cpp
@@ -7,14 +7,17 @@
 
 #include <iostream>
 #include "handle_args/HandleArgs.hpp"
+#include "display/Window.hpp"
 
 using namespace GUI;
 
 int main(int ac, char **av)
 {
     GUI::HandleArgs args;
+    GUI::Window win;
 
     if (args.checkArgs(ac, av))
         return 84;
+    win.displayWindow();
     return 0;
 }
