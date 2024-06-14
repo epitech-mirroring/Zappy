39009cd87d529e474db5b594570975232c6b2ebe
Bxptiste <bxptiste@baptistempc.home>
2024-06-14 10:52:09 +0200

    :construction: added the ICommand interface with callbacks

---
+++ gui/src/network/commands/interface/ICommand.hpp
@@ -0,0 +1,46 @@
+/*
+** EPITECH PROJECT, 2024
+** Zappy
+** File description:
+** ICommand
+*/
+
+#ifndef ICOMMAND_HPP_
+    #define ICOMMAND_HPP_
+
+    #include <functional>
+    #include <string>
+    #include <sstream>
+    #include <memory>
+
+namespace network {
+    class ICommand {
+    public:
+        ICommand() noexcept = default;
+        virtual ~ICommand() noexcept = default;
+
+        /**
+         * @brief Execute the command
+         * @param iss std::istringstream
+         */
+        virtual void execute(std::istringstream& iss) = 0;
+
+        /**
+         * @brief Clone the command
+         * @return std::unique_ptr<ICommand>
+         */
+        virtual std::unique_ptr<ICommand> clone() const = 0;
+
+        /**
+         * @brief Set the callback
+         * @param callback std::function<void(std::istringstream&)>
+         */
+        using Callback = std::function<void(std::istringstream&)>;
+        void setCallback(Callback callback) {
+            _callback = std::move(callback);
+        }
+    protected:
+        Callback _callback;
+    };
+} // namespace network //
+#endif /* !ICOMMAND_HPP_ */
