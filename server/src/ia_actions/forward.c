df6af5fbb6fd257310623c5772df34ca539da06d
Quentin Tennerel <quentin.tennerel@epitech.eu>
2024-06-19 16:56:50 +0200

    :sparkles: add forward ia command

--- server/src/ia_actions/forward.c
+++ server/src/ia_actions/forward.c
@@ -9,8 +9,59 @@
 #include "game.h"
 #include "actions.h"
 
+static void forward_north(game_t *game, trantorian_t *trantorian)
+{
+    if (trantorian->coordinates.y == 0) {
+        trantorian->coordinates.y = game->map->height - 1;
+    } else {
+        trantorian->coordinates.y -= 1;
+    }
+}
+
+static void forward_east(game_t *game, trantorian_t *trantorian)
+{
+    if (trantorian->coordinates.x == game->map->width - 1) {
+        trantorian->coordinates.x = 0;
+    } else {
+        trantorian->coordinates.x += 1;
+    }
+}
+
+static void forward_south(game_t *game, trantorian_t *trantorian)
+{
+    if (trantorian->coordinates.y == game->map->height - 1) {
+        trantorian->coordinates.y = 0;
+    } else {
+        trantorian->coordinates.y += 1;
+    }
+}
+
+static void forward_west(game_t *game, trantorian_t *trantorian)
+{
+    if (trantorian->coordinates.x == 0) {
+        trantorian->coordinates.x = game->map->width - 1;
+    } else {
+        trantorian->coordinates.x -= 1;
+    }
+}
+
 void forward(game_t *game, trantorian_t *trantorian)
 {
-    (void)game;
-    (void)trantorian;
+    char *msg = malloc(sizeof(char) * 4);
+
+    if (trantorian->direction == NORTH) {
+        forward_north(game, trantorian);
+    }
+    if (trantorian->direction == EAST) {
+        forward_east(game, trantorian);
+    }
+    if (trantorian->direction == SOUTH) {
+        forward_south(game, trantorian);
+    }
+    if (trantorian->direction == WEST) {
+        forward_west(game, trantorian);
+    }
+    sprintf(msg, "ok\n");
+    buffer_write(trantorian->client->buffer_answered, msg);
+    free(msg);
 }
