a4deece098bc02cfdff9bf38008b1641d102d72b
6im0n <simon.gl3135@orange.fr>
2024-06-21 11:16:46 +0200

    :construction: Wip on broadcast

---
+++ server/src/ia_actions/broadcast.c
@@ -0,0 +1,18 @@
+
+/*
+** EPITECH PROJECT, 2024
+** Project
+** File description:
+** No file there , just an epitech header example .
+** You can even have multiple lines if you want !
+*/
+
+#include "game.h"
+#include "actions.h"
+
+
+void broadcast(game_t *game, trantorian_t *trantorian)
+{
+    (void) game;
+    (void) trantorian;
+}
