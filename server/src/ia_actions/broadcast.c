aa0af35e7be8f9c847c962c4d7b3a7af8842d21b
6im0n <simon.gl3135@orange.fr>
2024-06-21 14:46:59 +0200

    :rotating_light: Correct lint error

--- server/src/ia_actions/broadcast.c
+++ server/src/ia_actions/broadcast.c
@@ -10,37 +10,49 @@
 #include "actions.h"
 #include <math.h>
 #include "ia_actions/broadcast.h"
+#include "gui.h"
+
+static coordinates_t v_pos(int pos, trantorian_t *sender,
+                           trantorian_t *receiver, size_t h, size_t w)
+{
+    int virtual_position[8][2] = {
+            {receiver->coordinates.x, receiver->coordinates.y - h},
+            {receiver->coordinates.x, receiver->coordinates.y + h},
+            {receiver->coordinates.x - w, receiver->coordinates.y},
+            {receiver->coordinates.x + w, receiver->coordinates.y},
+            {receiver->coordinates.x - w, receiver->coordinates.y - h},
+            {receiver->coordinates.x + w, receiver->coordinates.y - h},
+            {receiver->coordinates.x - w, receiver->coordinates.y + h},
+            {receiver->coordinates.x + w, receiver->coordinates.y + h}
+    };
+
+    return (coordinates_t){virtual_position[pos][0], virtual_position[pos][1]};
+
+}
 
 static coordinates_t get_closet_coordinates(trantorian_t *sender,
-                    trantorian_t *receiver, size_t height, size_t width)
+    trantorian_t *receiver, size_t h, size_t w)
 {
     int min_distance = 1000000;
     int distance;
     int closest_position[2] = {0, 0};
-    int virtual_position[8][2] = {
-            {receiver->coordinates.x, receiver->coordinates.y - height},
-            {receiver->coordinates.x, receiver->coordinates.y + height},
-            {receiver->coordinates.x - width, receiver->coordinates.y},
-            {receiver->coordinates.x + width, receiver->coordinates.y},
-            {receiver->coordinates.x - width, receiver->coordinates.y - height},
-            {receiver->coordinates.x + width, receiver->coordinates.y - height},
-            {receiver->coordinates.x - width, receiver->coordinates.y + height},
-            {receiver->coordinates.x + width, receiver->coordinates.y + height}
-    };
+
     for (int i = 0; i < 8; i++) {
-        distance = sqrt(pow(sender->coordinates.x - virtual_position[i][0], 2)
-                + pow(sender->coordinates.y - virtual_position[i][1], 2));
+        distance = sqrt(pow(sender->coordinates.x -
+                v_pos(i, sender, receiver, h, w).x, 2) +
+                pow(sender->coordinates.y -
+                v_pos(i, sender, receiver, h, w).y, 2));
         if (distance < min_distance) {
             min_distance = distance;
-            closest_position[0] = virtual_position[i][0];
-            closest_position[1] = virtual_position[i][1];
+            closest_position[0] = v_pos(i, sender, receiver, h, w).x;
+            closest_position[1] = v_pos(i, sender, receiver, h, w).y;
         }
     }
     return (coordinates_t){closest_position[0], closest_position[1]};
 }
 
 static int get_direction(trantorian_t receiver, trantorian_t *sender,
-                         coordinates_t closest)
+    coordinates_t closest)
 {
     if (receiver.coordinates.x == sender->coordinates.x &&
         receiver.coordinates.y == sender->coordinates.y)
@@ -74,7 +86,7 @@ static int orientation_correction(int sound_orientation)
 }
 
 static int sound_orientation(trantorian_t *sender, trantorian_t *receiver,
-                             size_t height, size_t width)
+    size_t height, size_t width)
 {
     int orientation = get_direction(*receiver, sender,
                     get_closet_coordinates(sender, receiver, height, width));
@@ -96,15 +108,19 @@ void broadcast(game_t *game, trantorian_t *trantorian)
     char *status_msg = calloc(10, sizeof(char));
 
     for (size_t i = 0; i < array_get_size(game->trantorians); i++) {
-        trantorian_t *receiver = (trantorian_t *)array_get_at(game->trantorians, i);
+        trantorian_t *receiver =
+                (trantorian_t *)array_get_at(game->trantorians, i);
         if (receiver->uuid != trantorian->uuid) {
-            int orientation = sound_orientation(trantorian, receiver, game->map->height, game->map->width);
-            snprintf(msg, 1024, "message %d, %s\n", orientation, trantorian->param);
+            int orientation = sound_orientation(trantorian, receiver,
+                                        game->map->height, game->map->width);
+            snprintf(msg, 1024, "message %d, %s\n",
+                     orientation, trantorian->param);
             buffer_write(receiver->client->buffer_answered, msg);
         }
     }
     free(msg);
     sprintf(status_msg, "ok\n");
     buffer_write(trantorian->client->buffer_answered, status_msg);
+    pbc_log_gui(game, trantorian, trantorian->param);
     free(status_msg);
 }
