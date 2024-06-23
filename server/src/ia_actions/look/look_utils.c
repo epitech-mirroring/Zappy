8eb24661e4f16260a76f2299a0df10a06e9326b3
6im0n <simon.gl3135@orange.fr>
2024-06-23 02:38:53 +0200

    :adhesive_bandage: Fix getelement on tile

--- server/src/ia_actions/look/look_utils.c
+++ server/src/ia_actions/look/look_utils.c
@@ -15,7 +15,7 @@ static char *add_space(char *str, char *tmp, int j, int element)
     return tmp;
 }
 
-static char *make_string_for_elements(const int *elements)
+static char *make_string_for_elements(hashmap_t *element)
 {
     char *str = calloc(1024, sizeof(char));
     char *tmp = calloc(1024, sizeof(char));
@@ -23,26 +23,44 @@ static char *make_string_for_elements(const int *elements)
         "sibur", "mendiane", "phiras", "thystame"};
 
     for (int i = 0; i < 8; i++) {
-        for (int j = 0; j < elements[i]; j++) {
+        for (int j = 0; j < hashmap_get(element, element_str[i]); j++) {
             sprintf(str, "%s%s", tmp, element_str[i]);
-            tmp = add_space(str, tmp, j, elements[i]);
+            tmp = add_space(str, tmp, j, hashmap_get(element, element_str[i]));
         }
     }
     free(str);
     return tmp;
 }
 
+static hashmap_t *init_tile_content(void)
+{
+    hashmap_t *resources;
+
+    resources = create_hashmap();
+    hashmap_put(resources, "player", 0);
+    hashmap_put(resources, "food", 0);
+    hashmap_put(resources, "linemate", 0);
+    hashmap_put(resources, "deraumere", 0);
+    hashmap_put(resources, "sibur", 0);
+    hashmap_put(resources, "mendiane", 0);
+    hashmap_put(resources, "phiras", 0);
+    hashmap_put(resources, "thystame", 0);
+    return resources;
+}
+
 char *get_element_on_tile(tile_t *tile)
 {
-    int elements[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+    hashmap_t *content = init_tile_content();
 
-    elements[0] = tile->player_count;
-    elements[1] = hashmap_get(tile->resources, "food");
-    elements[2] = hashmap_get(tile->resources, "linemate");
-    elements[3] = hashmap_get(tile->resources, "deraumere");
-    elements[4] = hashmap_get(tile->resources, "sibur");
-    elements[5] = hashmap_get(tile->resources, "mendiane");
-    elements[6] = hashmap_get(tile->resources, "phiras");
-    elements[7] = hashmap_get(tile->resources, "thystame");
-    return make_string_for_elements(elements);
+    hashmap_set(content, "player", tile->player_count);
+    hashmap_set(content, "food", hashmap_get(tile->resources, "food"));
+    hashmap_set(content, "linemate", hashmap_get(tile->resources, "linemate"));
+    hashmap_set(content, "deraumere",
+        hashmap_get(tile->resources, "deraumere"));
+    hashmap_set(content, "sibur", hashmap_get(tile->resources, "sibur"));
+    hashmap_set(content, "mendiane", hashmap_get(tile->resources, "mendiane"));
+    hashmap_set(content, "phiras", hashmap_get(tile->resources, "phiras"));
+    hashmap_set(content, "thystame", hashmap_get(tile->resources, "thystame"));
+
+    return make_string_for_elements(content);
 }
